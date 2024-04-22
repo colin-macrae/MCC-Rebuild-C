860b3effb1d1946dcdfc35c98b187861dfcf0bd7
Colin Macrae <csmacrae@gmail.com>
2024-04-22 14:58:45 -0700

    add specificity to css selectors with classnames to fix tailwind conflicts

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -4,8 +4,8 @@ export default function GetInTouch() {
   return (
     <section className="get-in-touch-section">
       <div className="get-in-touch-content">
-        <h2>Get In Touch</h2>
-        <p>
+        <h2 className="get-in-touch-heading">Get In Touch</h2>
+        <p className="get-in-touch-text">
           When it comes to commercial cleaning, Massachusetts businesses know
           MCC is the one to call. Let’s get started on ensuring your cleaning
           needs are being met.
