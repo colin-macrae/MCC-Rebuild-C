b4ea124262aafa8bc10259702a5e93f942f0b6bb
Colin Macrae <csmacrae@gmail.com>
2024-05-01 16:47:28 -0700

    add ROS to getInTouch.tsx and Employment page

--- src/components/GetInTouch.tsx
+++ src/components/GetInTouch.tsx
@@ -1,20 +1,23 @@
 import "./GetInTouch.css";
 import { Link } from "react-router-dom";
+import Reveal from "./Utils/Reveal";
+
 export default function GetInTouch() {
   return (
     <section className="get-in-touch-section get-in-touch-bg">
-      <div className="get-in-touch-content">
-        <h2 className="get-in-touch-heading">Get In Touch</h2>
-        <p className="get-in-touch-text">
-          When it comes to commercial cleaning, Massachusetts businesses know
-          MCC is the one to call. Let’s get started on ensuring your cleaning
-          needs are being met.
-        </p>
-        <Link to="/contact">
-          <button className="btn">CONTACT US &nbsp; &rsaquo;</button>
-        </Link>
-        
-      </div>
+      <Reveal>
+        <div className="get-in-touch-content">
+          <h2 className="get-in-touch-heading">Get In Touch</h2>
+          <p className="get-in-touch-text">
+            When it comes to commercial cleaning, Massachusetts businesses know
+            MCC is the one to call. Let’s get started on ensuring your cleaning
+            needs are being met.
+          </p>
+          <Link to="/contact">
+            <button className="btn">CONTACT US &nbsp; &rsaquo;</button>
+          </Link>
+        </div>
+      </Reveal>
     </section>
   );
 }
