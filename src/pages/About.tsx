fa986f2603ca6bcc814ab588cd2c7c8d1bed9fd7
James Aguirre <jamesaguirre98@gmail.com>
2024-05-06 20:42:24 -0700

    added map feature to home page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import "../assets/111-Thatcher-Day-1-roback-11.jpg";
 import "../assets/111-Thatcher-Day-1-roback-3.jpg";
 import GetInTouch from "../components/GetInTouch.tsx";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import Reveal from "../components/Utils/Reveal.tsx";
 
 const images = [
@@ -41,9 +41,9 @@ export default function About() {
           <div className="about-heading-text">
             <h2>A Team of Owners</h2>
             <p>
-              What makes MCC stand out from other cleaning companies in
-              Massachusetts is that we are employee-owned. When you do business
-              with us, you are directly supporting our cleaners and community.
+              What makes MCC stand out from other cleaning companies in Massachusetts is
+              that we are employee-owned. When you do business with us, you are directly
+              supporting our cleaners and community.
             </p>
             <Link to="/contact">
               <button className="btn">CONTACT US &rsaquo;</button>
@@ -57,15 +57,11 @@ export default function About() {
             <h2>Caring About Every Clean</h2>
             <p>
               As employee-owners, each MCC team member is trained in providing a
-              best-in-class cleaning service to every one of our many facilities
-              in the New England area. This means from your initial call for a
-              quote to each clean you have with us, every interaction is with an
-              owner of the company.
+              best-in-class cleaning service to every one of our many facilities in the
+              New England area. This means from your initial call for a quote to each
+              clean you have with us, every interaction is with an owner of the company.
             </p>
-            <button
-              className="btn"
-              onClick={() => setShowPhoneNumber(!showPhoneNumber)}
-            >
+            <button className="btn" onClick={() => setShowPhoneNumber(!showPhoneNumber)}>
               {showPhoneNumber ? "(413) 568-4696" : "CALL US NOW"}
             </button>
           </div>
@@ -98,57 +94,56 @@ export default function About() {
               ))}
             </div>
           </div>
-        
+
           <div className="about-caring-text">
             <p className="history-and-about">
-              Mass Commercial Cleaning, Inc. is an MA-based commercial cleaning
-              company founded in 1989. Our company has grown from a two-person
-              start-up to over one hundred employees. In 2021, MCC transitioned
-              to employee ownership after the retirement of its founders. Today,
-              we are an employee-owned company where all employees receive
-              company shares and participate in profit distribution.
+              Mass Commercial Cleaning, Inc. is an MA-based commercial cleaning company
+              founded in 1989. Our company has grown from a two-person start-up to over
+              one hundred employees. In 2021, MCC transitioned to employee ownership after
+              the retirement of its founders. Today, we are an employee-owned company
+              where all employees receive company shares and participate in profit
+              distribution.
             </p>
             <p className="history-and-about">
-              We’re dedicated to ensuring that from our supply managers to our
-              quality control, we’re providing our employee-owners with the
-              right tools and supplies to effectively do their job. Going so far
-              as to utilize a janitorial cleaning time tracking and task
-              tracking mobile application, we ease communication between MCC and
-              clients to ensure you’re always aware of your cleaning status.
+              We’re dedicated to ensuring that from our supply managers to our quality
+              control, we’re providing our employee-owners with the right tools and
+              supplies to effectively do their job. Going so far as to utilize a
+              janitorial cleaning time tracking and task tracking mobile application, we
+              ease communication between MCC and clients to ensure you’re always aware of
+              your cleaning status.
             </p>
             <p className="history-and-about">
-              Our management team has a combined 100+ years in the cleaning
-              industry. Having employees who have been with us for over two
-              decades is a testament to the positive environment we create for
-              both our employees and our clients.
+              Our management team has a combined 100+ years in the cleaning industry.
+              Having employees who have been with us for over two decades is a testament
+              to the positive environment we create for both our employees and our
+              clients.
             </p>
           </div>
         </Reveal>
-<Reveal>
-        <div>
-          
+        <Reveal>
+          <div>
             <img
               src="https://masscocleaning.wpengine.com/wp-content/uploads/DSC_9794.jpg"
               alt="mass comm team posing"
               className="team-pic-single"
             />
-        </div>
-        <div className="pics-double-container">
-          <div>
+          </div>
+          <div className="pics-double-container">
+            <div>
               <img
                 src="https://masscocleaning.wpengine.com/wp-content/uploads/DSC_9878@2x.jpg"
                 alt="mass comm team posing"
                 className="team-pic-double"
               />
-            <img
-              src="https://masscocleaning.wpengine.com/wp-content/uploads/DSC_9858@2x.jpg"
-              alt="mass comm team posing"
-              className="team-pic-double"
-            />
+              <img
+                src="https://masscocleaning.wpengine.com/wp-content/uploads/DSC_9858@2x.jpg"
+                alt="mass comm team posing"
+                className="team-pic-double"
+              />
+            </div>
           </div>
-        </div></Reveal>
+        </Reveal>
       </section>
-
       <GetInTouch />
     </div>
   );
