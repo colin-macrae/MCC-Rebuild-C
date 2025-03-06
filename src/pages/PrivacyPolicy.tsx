6673c1328a838b604676cc013479e86ab30d865a
colin-macrae <csmacrae@gmail.com>
2025-03-05 18:38:36 -0800

    fix height, make some text bold

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -6,28 +6,31 @@ import './Services-Pages/ServicesPagesTemplate.css';
 export default function PrivacyPolicy() {
   return (
     <div className="min-h-[40rem]">
-      <section className="heading-section min-h-[40rem]">
+      <section className="min-h-[40rem]">
         <div className="heading-text max-w-[1400px] text-left">
           <h2 className="mb-5">Privacy Policy</h2>
           <p className="mb-5">
-            Phone Number Privacy Policy: We respect your privacy and are
-            committed to protecting your personal information.
+            <strong>Phone Number Privacy Policy:</strong> We respect your
+            privacy and are committed to protecting your personal information.
           </p>
           <p className="mb-5">
-            Use of Information: We use your phone number to send SMS messages
-            regarding your inquiry and customer care related topics. Mobile
-            phone information or opt-in data collected will not be shared, sold,
-            or disclosed to third parties or affiliates for marketing purposes.
+            <strong>Use of Information:</strong> We use your phone number to
+            send SMS messages regarding your inquiry and customer care related
+            topics. Mobile phone information or opt-in data collected will not
+            be shared, sold, or disclosed to third parties or affiliates for
+            marketing purposes.
           </p>
           <p className="mb-5">
-            Opt-Out: Reply "STOP" to unsubscribe or "HELP" for assistance.
+            <strong>Opt-Out:</strong> Reply "STOP" to unsubscribe or "HELP" for
+            assistance.
           </p>
           <p className="mb-5">
-            Message Disclosures: Message frequency varies. Message and data
-            rates may apply.
+            <strong>Message Disclosures:</strong> Message frequency varies.
+            Message and data rates may apply.
           </p>
           <p className="mb-5">
-            Data Security: Your data is protected and retained only as needed.
+            <strong>Data Security:</strong> Your data is protected and retained
+            only as needed.
           </p>
         </div>
       </section>
