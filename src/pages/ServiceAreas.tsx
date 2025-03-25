3dd8a7c5ccc2f33a832b0a81b55393d96d16bca6
colin-macrae <csmacrae@gmail.com>
2025-03-25 16:55:00 +0800

    adjust service areas pages' framer motion divs for more consistent and aesthetic reveals

--- src/pages/ServiceAreas.tsx
+++ src/pages/ServiceAreas.tsx
@@ -1,13 +1,13 @@
-import Reveal from "../components/Utils/Reveal";
-import GetInTouch from "../components/GetInTouch";
-import "./ServiceAreas.css";
+import Reveal from '../components/Utils/Reveal';
+import GetInTouch from '../components/GetInTouch';
+import './ServiceAreas.css';
 
 export default function ServiceAreas() {
   return (
     <div>
-      <Reveal>
-        <section className="heading-section">
-          <div className="heading-text max-w-[1400px]">
+      <section className="heading-section">
+        <div className="heading-text max-w-[1400px]">
+          <Reveal>
             <h2>Service Areas</h2>
             <p>
               Discover MCC's reliable commercial cleaning services covering the
@@ -15,10 +15,12 @@ export default function ServiceAreas() {
               delivering exceptional cleaning solutions to businesses in these
               vibrant communities.
             </p>
-          </div>
-        </section>
-        <section className="intro">
-          <div className="intro-container">
+          </Reveal>
+        </div>
+      </section>
+      <section className="intro">
+        <div className="intro-container">
+          <Reveal>
             <img
               className="intro-image MA-img"
               src="/service-areas-map.gif"
@@ -36,7 +38,10 @@ export default function ServiceAreas() {
                 elevate customer satisfaction.
               </p>
             </div>
-          </div>
+          </Reveal>
+        </div>
+
+        <Reveal>
           <div className="cities-container">
             <div className="row-cities">
               <ul>
@@ -84,23 +89,29 @@ export default function ServiceAreas() {
               </ul>
             </div>
           </div>
-        </section>
-        <section className="section-three">
-          <div className="section-three-container">
+        </Reveal>
+      </section>
+      <section className="section-three">
+        <div className="section-three-container">
+          <Reveal>
             <h2 className="section-three-heading">
               Tailored Cleaning Solutions for Every Facility
             </h2>
             <p className="section-three-text">
-              Contacting our commercial cleaning company grants you access to a comprehensive suite of services essential for maintaining your office in impeccable condition. As a dedicated janitorial and
+              Contacting our commercial cleaning company grants you access to a
+              comprehensive suite of services essential for maintaining your
+              office in impeccable condition. As a dedicated janitorial and
               commercial cleaning service provider, we prioritize quality to
               alleviate your stress and streamline operations. Our tailored
               approach includes commercial cleaning, janitorial services,
               commercial carpet cleaning, and office cleaning. These services
-              encompass a range of solutions from office cleaning to janitorial maintenance, ensuring every aspect of your commercial property shines with professionalism and cleanliness.
+              encompass a range of solutions from office cleaning to janitorial
+              maintenance, ensuring every aspect of your commercial property
+              shines with professionalism and cleanliness.
             </p>
-          </div>
-        </section>
-      </Reveal>
+          </Reveal>
+        </div>
+      </section>
       <GetInTouch />
     </div>
   );
